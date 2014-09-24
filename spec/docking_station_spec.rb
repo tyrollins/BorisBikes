7ba0a8879823a32ab9ad2f7ccd9e6e8f5fee64b0
stoked <tylerrollins@outlook.com>
2014-09-23 20:37:21 -0700

    added default capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ require 'bike'
 describe DockingStation do
 
   let(:bike) {Bike.new}
-  let(:station) {DockingStation.new}
+  let(:station) {DockingStation.new(:capacity => 20)}
 
   it 'should be able to accept bike' do
     expect(station.bike_count).to eq(0)
@@ -18,4 +18,9 @@ describe DockingStation do
     expect(station.bike_count).to eq(0)
   end
 
+  it 'should know when it is full' do
+    expect(station).not_to be_full
+    20.times {station.dock(Bike.new)}
+    expect(station).to be_full
+  end
 end
