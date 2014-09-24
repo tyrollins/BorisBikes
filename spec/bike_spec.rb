127ee4182f2cdee133c628835d38a56267110423
stoked <tylerrollins@outlook.com>
2014-09-23 18:20:55 -0700

    bike can break / tests pass

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,11 @@ describe Bike do
     the_bike = Bike.new
     expect(the_bike).not_to be_broken
   end
+
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break!
+    expect(bike).to be_broken
+  end
+
 end
